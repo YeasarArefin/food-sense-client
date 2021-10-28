3315940e50a5f72049b0fdb4796789e0d444ca6d
YeasarArefin <yeasararefin007@gmail.com>
2021-10-29 03:18:14 +0600

    app section added

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Ourapp from './Ourapp';
 
 const HomePage = () => {
     return (
         <>
 
             <Header />
+            <Ourapp />
 
         </>
     );
